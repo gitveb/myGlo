dd92fd6a9cf03c93990d0982978f5d43a4e17af1
Alexei Bethinenko <vebsite100@yandex.ru>
2025-01-14 17:10:16 +0400

    Выполненный четвертый урок

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-		
+'use strict'
+
+let title = prompt ("Как называется ваш проект?");
+let screens = prompt ("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt ("Сколько будет стоить данная работа?", "12000");
+let rollback = 50;
+let service1 = prompt ("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt ("Сколько это будет стоить");
+let service2 = prompt ("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt ("Сколько это будет стоить");
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let adaptive = confirm ("Нужен ли адаптив на сайте?");
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+
+switch (true) {
+	case fullPrice >= 30000:
+		console.log("Даем скидку в 10%");
+		break
+	case fullPrice >= 15000 && fullPrice < 30000:
+		console.log("Даем скидку в 5%");
+		break
+	case fullPrice < 15000 && fullPrice > 0:
+		console.log("Скидка не предусмотрена");
+		break
+	case fullPrice <= 0:
+		console.log("Что то пошло не так");
+}		
